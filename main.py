85346373377be1b0338ca7e82a49363f17078357
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-09-09 11:24:15 -0500

    Implementacion division

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
+def divicion(x,y):
+    return x/y
 
 def resta(x,y):
     return x-y
 
 respuesta = resta(6,3)
+print("resultado es: " + str(respuesta))
+respuesta = divicion(6,3)
 print("resultado es: " + str(respuesta))
