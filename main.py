63925d98648f9ab672e6a99067423fd5170f4358
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-09-09 10:59:25 -0500

    Eliminacion de sumas

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def suma(x,y):
-    return x+y
 
 
 def resta(x,y):
     return x-y
 
-respuesta = suma(2,3)
-print("resultado es: " + str(respuesta))
 respuesta = resta(6,3)
 print("resultado es: " + str(respuesta))
