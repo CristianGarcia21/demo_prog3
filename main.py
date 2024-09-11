46c8191a63790da3353fcb9ee96da6774240be62
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-09-11 07:35:28 -0500

    Implementacion de la suma

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ def divicion(x,y):
 def resta(x,y):
     return x-y
 
+def suma(x,y):
+    return x+y
+
 respuesta = resta(6,3)
 print("resultado es: " + str(respuesta))
 respuesta = divicion(6,3)
 print("resultado es: " + str(respuesta))
+resultadoSuma = suma(8,9)
+print("resultado es: " + str(resultadoSuma))
