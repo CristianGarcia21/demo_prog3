3cc4d7e28e25cf50bd516cc226dae4834945a23a
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-09-11 07:40:13 -0500

    Solucion division

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def resta(x,y):
 
 respuesta = resta(6,3)
 print("resultado es: " + str(respuesta))
-respuesta = divicion(6,3)
-print("resultado es: " + str(respuesta))
+respuestaDivicion = divicion(6,3)
+print("resultado es: " + str(respuestaDivicion))
